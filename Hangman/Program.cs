99f58d13b51f2c0d40b73b8010783ae118bc493e
George Sydby <george.sydby@gmail.com>
2021-09-27 11:50:11 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+
+
+            Console.WriteLine(SecretWords.SecretWord());
+
+            Console.WriteLine("\nProgram finished");
+        }
+    }
+}
